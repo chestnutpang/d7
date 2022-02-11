b92b9df20f827098c5b7b5fb92a20f323748df50
Zizhuo Pang <zizhuo.pang@corerain.com>
2022-02-11 18:30:55 +0800

    d14 complete

---
+++ d14/geecache/peers.go
@@ -0,0 +1,15 @@
+package geecache
+
+
+import pb "geecache/geecachepb"
+
+
+type PeerPicker interface {
+	PickPeer(key string) (peer PeerGetter, ok bool)
+}
+
+
+type PeerGetter interface {
+	Get(in *pb.Request, out *pb.Response) error
+}
+
