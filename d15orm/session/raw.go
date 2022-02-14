fefe2c2f5ed06e650be2e1bb111c60d3eaaabeef
Zizhuo Pang <zizhuo.pang@corerain.com>
2022-02-14 19:15:25 +0800

    add d15

---
+++ d15orm/session/raw.go
@@ -0,0 +1,18 @@
+package session
+
+import (
+	"database/sql"
+	"geeorm/log"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
